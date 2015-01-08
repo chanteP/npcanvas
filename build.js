02449a9060e2741166e7dc2be1b8d8d03510bca9
chenwenbin03 <chenwenbin03@meituan.com>
2015-01-08 18:41:42 +0800

    min

--- build.js
+++ build.js
@@ -3,6 +3,14 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 var fs = require('fs');
 
+var dest = './dist/npcanvas.js';
+var destmin = './dist/npcanvas.min.js';
+
 var b = browserify();
 b.add('./index.js');
-b.bundle().pipe(fs.createWriteStream('./dist/npcanvas.js'));
+b.bundle()
+    .pipe(fs.createWriteStream(dest))
+    .on('finish', function(){
+        var content = uglify.minify(dest).code;
+        fs.writeFile(destmin, content);
+    });
